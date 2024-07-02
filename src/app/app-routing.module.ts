399e5dc2890b960d6aa95d4787b13a719f52d696
whereismyname7 <kalshmrani70@hotmail.com>
2024-07-02 15:12:39 +0300

    Feat: login UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path : 'login', component: LoginComponent},
+  {path : '', component: LoginComponent},
   {path : 'home', component: HomeComponent}
 ];
 
